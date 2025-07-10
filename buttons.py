977b35ffa30370fd5b2df4c2e99b1944de179b81
MatTerrezza <72928801+MatTerrezza@users.noreply.github.com>
2025-07-10 11:39:37 +0300

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -4,12 +4,32 @@ import os
 
 def get_main_keyboard():
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    
     btn_form = types.KeyboardButton("Оформить заказ через форму")
     btn_me = types.KeyboardButton("Связаться со мной")
+    btn_service = types.KeyboardButton("Услуги")
     
+    keyboard.add(btn_service)
     keyboard.add(btn_form, btn_me)
     return keyboard
 
+def get_inline_menu():
+    markup = types.InlineKeyboardMarkup()
+    
+    btn1 = types.InlineKeyboardButton("🤖 Telegram-боты", callback_data='service_bots')
+    btn2 = types.InlineKeyboardButton("📱 Mini Apps", callback_data='service_miniapps')
+    btn3 = types.InlineKeyboardButton("🛠 Настройка CRM", callback_data='service_crm')
+    btn4 = types.InlineKeyboardButton("🐍 Python-скрипты", callback_data='service_scripts')    
+    
+    markup.add(btn1, btn2, btn3, btn4)
+    return markup
+
+def get_back_button():
+    markup = types.InlineKeyboardMarkup()
+    btn_back = types.InlineKeyboardButton("🔙 Назад", callback_data='back_to_main')
+    markup.add(btn_back)
+    return markup
+    
 def get_inline_keyboard():
     inline_keyboard = types.InlineKeyboardMarkup()
     btn_lform = types.InlineKeyboardButton(
